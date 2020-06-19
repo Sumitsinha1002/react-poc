aae2eb68dac41c692e65cc7d8fd0c3c9e260d6c4
Sumit <sumit@ionoview.com>
2020-06-19 12:04:55 +0400

    Updates

---
+++ precache-manifest.abd8e12bebee7201531bcfd6abbc1c96.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e74bb9c42723f0aee38338adcbf5117",
+    "url": "/react-poc/index.html"
+  },
+  {
+    "revision": "ffba7cf600cfea5b8718",
+    "url": "/react-poc/static/css/main.9de36969.chunk.css"
+  },
+  {
+    "revision": "e3ce1d1dd945f552a148",
+    "url": "/react-poc/static/js/2.fe32c892.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-poc/static/js/2.fe32c892.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffba7cf600cfea5b8718",
+    "url": "/react-poc/static/js/main.d045411a.chunk.js"
+  },
+  {
+    "revision": "d093005e8b2045abaead",
+    "url": "/react-poc/static/js/runtime-main.88cc7397.js"
+  },
+  {
+    "revision": "cb655b876a2433f03316d7a3793ce5b2",
+    "url": "/react-poc/static/media/goodfoot.cb655b87.ttf"
+  },
+  {
+    "revision": "5558d33b676596fa5cda52be372037f8",
+    "url": "/react-poc/static/media/no-data.5558d33b.jpg"
+  }
+]);
